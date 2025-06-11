0c1978bff994c2d7eddf8603ca0ab1f0c23cda68
dasom222g <dasom228@gmail.com>
2025-06-11 16:06:54 +0900

    feat: Add packages

---
+++ crawler/fetcher.py
@@ -0,0 +1,27 @@
+# import requests
+
+# from common.data import parking_list_api_url
+
+
+# def fetch_parking_list():
+#
+#     headers = {
+#         "User-Agent": "Mozilla/5.0",
+#         "Referer": "https://new-m.pay.naver.com",
+#         "Accept": "application/json",
+#     }
+#
+#     response = requests.get(parking_list_api_url, headers=headers)
+#     data = response.json()
+#     print(f"data: {data}")
+
+
+
+def fetch():
+    # 파킹통장 상품 리스트 크롤링
+    # fetch_parking_list()
+    print('dkskl')
+    # 각 파킹통장 detail
+
+if __name__ == '__main__':
+    fetch()
