6a75b1b5b3f9b28e923119d0057d3effb2f94d12
dasom222g <dasom228@gmail.com>
2025-06-22 14:01:59 +0900

    refactoring

--- common/enums.py
+++ common/enums.py
@@ -1,11 +1,15 @@
 from enum import Enum
 
+
 class DocumentTypeEnum(str, Enum):
     """벡터화할 문서 타입을 구분하는 Enum"""
-    FULL = 'full'
-    CHUNKS = 'chunks'
+
+    FULL = "full"
+    CHUNKS = "chunks"
+
 
 class EmbeddingModelEnum(str, Enum):
     """임베딩 모델 타입을 구분하는 Enum"""
-    TEXT_EMBEDDING_SMALL = 'text-embedding-3-small'
-    TEXT_EMBEDDING_LARGE = 'text-embedding-3-large'
+
+    TEXT_EMBEDDING_SMALL = "text-embedding-3-small"
+    TEXT_EMBEDDING_LARGE = "text-embedding-3-large"
